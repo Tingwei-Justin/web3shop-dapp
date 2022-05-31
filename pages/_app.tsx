ae7fd7b696237c1e4bbd7004c8e7744027b0616c
Tingwei Zhou <zhoutingwei1996@gmail.com>
2022-05-31 18:47:38 -0400

    update file position

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,37 @@
-import '../styles/globals.css'
+import '@assets/main.css'
+import '@assets/chrome-bug.css'
+import 'keen-slider/keen-slider.min.css'
+
+import { FC, useEffect } from 'react'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import { Head } from '@components/common'
+import { ManagedUIContext } from '@components/ui/context'
 import { MoralisProvider } from 'react-moralis'
+import { RecoilRoot } from "recoil";
 
+const Noop: FC = ({ children }) => <>{children}</>
 
-function MyApp({ Component, pageProps }: AppProps) {
-  const appID = "https://nszopjl5luya.usemoralis.com:2053/server" //process.env.MORALIS_APP_ID;
-  const serverUrl = "lCcURyqUP0QTmNdi8qwZp67Sq1IUkLM4xlZeDLYw" //process.env.MORALIS_SERVER_URL;
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const Layout = (Component as any).Layout || Noop
+
+  useEffect(() => {
+    document.body.classList?.remove('loading')
+  }, [])
+
   return (
-    <div>
-      <Head>
-        <link rel="shortcut icon" href="/logo.jpg" />
-      </Head>
-      <MoralisProvider
-        serverUrl="https://ewfzwowhbm1w.usemoralis.com:2053/server"
-        appId="3fGgHqtRsUXYlfed76R1L2GoFYkvB5mcZWAYINyz"
-      >
-        <Component {...pageProps} />
-      </MoralisProvider>
-    </div >
+    <MoralisProvider
+      serverUrl="https://nszopjl5luya.usemoralis.com:2053/server"
+      appId="lCcURyqUP0QTmNdi8qwZp67Sq1IUkLM4xlZeDLYw"
+    >
+      <RecoilRoot>
+        <Head />
+        <ManagedUIContext>
+          <Layout pageProps={pageProps}>
+            <Component {...pageProps} />
+          </Layout>
+        </ManagedUIContext>
+      </RecoilRoot>
+
+    </MoralisProvider>
   )
 }
-
-export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,202 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { GetStaticPropsContext } from 'next'
+import { Layout } from '@components/common'
+import { Text } from '@components/ui'
 import Image from 'next/image'
-import tw from 'twin.macro'
-import Link from 'next/link'
+import { PlusSmIcon as PlusSmIconSolid } from '@heroicons/react/solid'
+import Masonry from '@components/common/Masonry'
+import { useRecoilState, useRecoilValue, useRecoilValueLoadable } from 'recoil'
+import { userAllInfoSelector, userAddress, userFollowingCommunitiesPostSelector, rankingUsersInWeb3Graph } from 'states/user'
 import { useEffect, useState } from 'react'
-import { useMoralis, useMoralisWeb3Api, useWeb3Contract, useWeb3ExecuteFunction } from "react-moralis";
-import { abi, bytecode } from "../data/abi"
+import { useMoralis } from 'react-moralis'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import PostButton from '@components/post/PostButton'
+import { getUersPosts } from '@lib/follow'
+import { client, TopNRankingsQuery } from '@lib/cyberconnect-query'
+import RankingUsers from '@components/user/RankingUsers'
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material'
 
-const Home: NextPage = () => {
-    const [showAll, setShowAll] = useState(false);
-    const { web3, Moralis, enableWeb3, isWeb3Enabled, authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
 
-    // const myContract = new ethers.ContractFactory(abi, bytecode, signer)
+export async function getStaticProps({ }: GetStaticPropsContext) {
+    const res = await fetch(`${process.env.DOMAIN_URL}/api/post`)
+    const result = await res.json()
+    if (!res.ok || !result || !result.success) {
+        return {
+            notFound: true,
+        };
+        // throw new Error(`Failed to fetch products, received status ${res.status}`);
+    }
+    let posts = [];
+    if (result?.data?.length > 0) {
+        posts = result.data.filter((post) => post?.data?.imageUrl?.length > 0)
+    }
+    return {
+        props: {
+            posts
+        },
+        revalidate: 5,
+    }
+}
+
+const defaultTabs = [
+    { name: 'Hot', href: '#', current: true },
+    { name: 'Followed', href: '#', current: false },
+    { name: 'My', href: '#', current: false },
+]
+
+export function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+}
+
+export default function Home({ posts }) {
+    const [address, setAddress] = useRecoilState<string>(userAddress);
+    const userFollowingsPostLoadable = useRecoilValueLoadable(userFollowingCommunitiesPostSelector);
+    // const [followingPosts, setFollowingPosts] = useRecoilState(userFollowingCommunitiesPostSelector);
+    const [tabs, setTabs] = useState(defaultTabs);
+    const [displayedPosts, setDisplayedPosts] = useState(posts);
+    const [renderPosts, setRenderPosts] = useState(posts);
+    const [displayedPostsBySelection, setDisplayedPostsBySelection] = useState(posts);
+    const [userPosts, setUserPosts] = useState([]);
+    const [userFollowingPosts, setUserFollowingPosts] = useState([]);
+    const router = useRouter();
+    // const user = useRecoilValue(userAllInfoSelector);
+    const userLoadable = useRecoilValueLoadable(userAllInfoSelector);
+    const { isAuthenticated, user: web3user } = useMoralis();
+    const [rankingUsers, setRankingUsers] = useRecoilState(rankingUsersInWeb3Graph);
+    const [postType, setPostType] = useState("all");
 
 
     useEffect(() => {
+        let postsResult = [];
+        if (postType === "design") {
+            postsResult = displayedPosts.filter((post) => post?.data?.isDesign)
+        } else if (postType === "social") {
+            postsResult = displayedPosts.filter((post) => !post?.data?.isDesign)
+        } else {
+            postsResult = displayedPosts;
+        }
+        setRenderPosts(postsResult);
+    }, [postType, displayedPosts, setRenderPosts])
 
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [isAuthenticated]);
+    // get the top 20 users for suggestion
+    useEffect(() => {
+        const variables = {
+            first: 50,
+        };
+        client
+            .request(TopNRankingsQuery, variables)
+            .then((res) => {
+                // console.log(res);
+                if (res?.rankings?.list?.length > 0) {
+                    // console.log(res.rankings.list);
+                    setRankingUsers(res.rankings.list)
+                }
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    }, [setRankingUsers]);
 
-    const deploy = async () => {
-        if (isAuthenticated && isWeb3Enabled) {
-            const ethers = Moralis.web3Library;
-            const signer = web3?.getSigner();
-            const myContract = new ethers.ContractFactory(abi, bytecode, signer);
-            console.log(myContract);
-            // If your contract requires constructor args, you can specify them here
-            const contract = await myContract.deploy("test", "test", "google");
+    useEffect(() => {
+        console.log("displayedPosts", displayedPosts);
+    }, [displayedPosts]);
 
-            console.log("contract address", contract.address);
-            console.log("trasaction record", contract.deployTransaction);
+    async function onHandleTab(idx) {
+        let newTabs = [...tabs];
+        for (let i = 0; i < newTabs.length; ++i) {
+            newTabs[i].current = i === idx;
+        }
+        setTabs(newTabs);
+        // TODO: LOADING STATE
+        if (newTabs[idx].name === "My" && address.length > 0) {
+            const currPosts = await getUersPosts(address);
+            setDisplayedPosts(currPosts);
+        } else if (newTabs[idx].name === "Followed" && address.length > 0) {
+            setDisplayedPosts(userFollowingPosts);
+        }
+        else {
+            setDisplayedPosts(posts);
         }
     }
-    const login = async () => {
-        if (!isAuthenticated) {
 
-            await authenticate({ signingMessage: "Log in using Moralis" })
-                .then(function (user) {
-                    console.log("logged in user:", user);
-                    console.log(user!.get("ethAddress"));
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
+    useEffect(() => {
+        if (userFollowingsPostLoadable.state === "hasValue") {
+            const followingPosts = userFollowingsPostLoadable.contents;
+            setUserFollowingPosts(followingPosts);
         }
-    }
+    }, [userFollowingsPostLoadable.state, userFollowingsPostLoadable.contents]);
 
-    const logOut = async () => {
-        await logout();
-        console.log("logged out");
-    }
+    function handlePostTypeChange(event) {
+        setPostType(event.target.value);
+    };
+
+    // useEffect(() => {
+    //     if (userLoadable.state === "hasValue") {
+    //         const userInfo = userLoadable.contents;
+    //         if (address && userInfo && !userInfo?.regesteredUser) {
+    //             router.push("/community");
+    //         }
+    //     }
+    // }, [userLoadable.state, userLoadable.contents, router, address]);
+
     return (
-        <div>
-            <Head>
-                <title>Web3Shop</title>
-                <meta name="description" content="Web3Shop" />
-            </Head>
-            {/* page container */}
-            <div tw="max-w-7xl mx-auto flex flex-col gap-8">
-                <div tw="text-blue-500">
-                    hello web3shop
-                </div>
-                <div>
-                    <button onClick={login}>Moralis Metamask Login</button>
+        <div className="w-screen">
+            <Image
+                className=""
+                alt="banner"
+                src="/test/banner.png"
+                layout="responsive"
+                width={1920}
+                height={522}
+            />
+            <div className='max-w-7xl mx-auto flex flex-col items-center justify-center mt-4'>
+                <div className="w-full flex justify-between items-center px-4">
+                    <RankingUsers />
+                    <nav className="-mb-px flex space-x-8 self-center" aria-label="Tabs">
+                        <FormControl fullWidth sx={{ width: "8rem" }} variant="standard">
+                            <InputLabel id="post-select-label">Post type</InputLabel>
+                            <Select
+                                sx={{ color: 'black', backgroundColor: 'white', borderColor: 'black' }}
+                                labelId="postType-select"
+                                value={postType}
+                                label="postType"
+                                onChange={handlePostTypeChange}
+                            >
+                                <MenuItem value="all">All</MenuItem>
+                                <MenuItem value="design">Design</MenuItem>
+                                <MenuItem value="social">Social</MenuItem>
+                            </Select>
+                        </FormControl>
+
+                        {tabs.map((tab, idx) => (
+                            <a
+                                key={tab.name}
+                                href={tab.href}
+                                onClick={() => { onHandleTab(idx) }}
+                                className={classNames(
+                                    tab.current
+                                        ? 'border-black text-black font-bold'
+                                        : 'border-transparent opacity-60 hover:opacity-80',
+                                    'whitespace-nowrap py-4 px-1 border-b-[3px] font-medium text-sm'
+                                )}
+                                aria-current={tab.current ? 'page' : undefined}
+                            >
+                                {tab.name}
+                            </a>
+                        ))}
+                    </nav>
+
+                    <div className='flex gap-2'>
+                        <PostButton />
+                    </div>
                 </div>
 
-                <div>
-                    <button onClick={logOut} disabled={isAuthenticating}>Logout</button>
-                </div>
-
-
-                <button tw=" bg-red-400 text-white" onClick={deploy}>
-                    Deploy
-                </button>
+                <section className='px-2 mx-auto my-12 w-full'>
+                    <Masonry posts={renderPosts} />
+                </section>
             </div>
-
-
-        </div >
+        </div>
     )
 }
-export default Home
+
+Home.Layout = Layout
